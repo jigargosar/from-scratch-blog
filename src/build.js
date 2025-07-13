1cd451678b5c2eb4f3e87a8664dea25380ee6eeb
Jigar Gosar <jigar.gosar@gmail.com>
2025-07-13 13:53:53 +0530

    Refactor index.html for improved readability; enhance build script to convert Markdown posts to HTML

--- src/build.js
+++ src/build.js
@@ -4,6 +4,7 @@
 const pug = require('pug');
 const fs = require('fs-extra');
 const path = require('path');
+const commonmark = require('commonmark');
 
 // Paths
 const srcDir = path.join(__dirname);
@@ -15,8 +16,26 @@ const indexHtml = path.join(outDir, 'index.html');
 fs.ensureDirSync(outDir);
 
 // Compile index.pug to HTML
-const html = pug.renderFile(indexPug);
+const html = pug.renderFile(indexPug, { pretty: true });
 fs.writeFileSync(indexHtml, html);
+console.log('index.pug processed and index.html generated in docs/');
+
+// Markdown to HTML for posts
+const postsSrcDir = path.join(srcDir, 'posts');
+const postsOutDir = path.join(outDir, 'posts');
+fs.ensureDirSync(postsOutDir);
+const postFiles = fs.readdirSync(postsSrcDir).filter(f => f.endsWith('.md'));
+const reader = new commonmark.Parser();
+const writer = new commonmark.HtmlRenderer();
+
+postFiles.forEach(file => {
+  const mdPath = path.join(postsSrcDir, file);
+  const mdContent = fs.readFileSync(mdPath, 'utf8');
+  const parsed = reader.parse(mdContent);
+  const htmlContent = writer.render(parsed);
+  const outPath = path.join(postsOutDir, file.replace(/\.md$/, '.html'));
+  fs.writeFileSync(outPath, htmlContent);
+  console.log(`${file} converted to HTML in docs/posts.`);
+});
 
 console.log('Build script ready. Add your build logic here.');
-console.log('index.pug processed and index.html generated in docs/');
